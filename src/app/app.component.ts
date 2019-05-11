ce392d13ae9193283156523f242aee7485b12780
Rogério F. Franco <rgffranco@gmail.com>
2019-05-11 18:47:33 -0300

    awesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,24 +4,28 @@ import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
 import {HighlightedDirective} from './directives/highlighted.directive';
 import {Observable} from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
 
+  courses$ : Observable<Course[]>;
 
-  courses = COURSES;
-
-  constructor() {
-
+  constructor(private http: HttpClient) {
   }
 
   ngOnInit() {
+    const params = new HttpParams()
+      .set("page", "1")
+      .set("pageSize", "10"));
+
+    this.courses$ = this.http.get<Course[]>('/api/courses', {params});
   }
-
-
-
+  
 }
