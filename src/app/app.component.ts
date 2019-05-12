8b1fd6ea57d61fc6be0736973046b672ec74bdd4
Rogério F. Franco <rgffranco@gmail.com>
2019-05-11 22:22:04 -0300

    awesome custom service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import {CourseCardComponent} from './course-card/course-card.component';
 import {HighlightedDirective} from './directives/highlighted.directive';
 import {Observable} from 'rxjs';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { CoursesService } from './services/courses.service';
 
 
 @Component({
@@ -17,15 +18,13 @@ export class AppComponent implements OnInit {
 
   courses$ : Observable<Course[]>;
 
-  constructor(private http: HttpClient) {
+  constructor(private courseService: CoursesService) {
   }
 
   ngOnInit() {
-    const params = new HttpParams()
-      .set("page", "1")
-      .set("pageSize", "10"));
 
-    this.courses$ = this.http.get<Course[]>('/api/courses', {params});
+    this.courses$ = this.courseService.loadCourses();
+
   }
   
 }
